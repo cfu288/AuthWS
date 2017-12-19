fad8bbe48213ac0993dd10af7d42d0ff21b98d06
Christopher Fu <cfu6@binghamton.edu>
2017-12-19 23:14:54 +0000

    further restructured, cleaned up comments. need to test

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ const UNAUTHORIZED = 401;
 const NOT_FOUND = 404;
 const SERVER_ERROR = 500;
 
+//________________________________RUN SERVER________________________________
+
 function serve(port, authTimeout, sslDir, model) {
     const app = express();
     app.locals.model = model;
@@ -30,11 +32,129 @@ function serve(port, authTimeout, sslDir, model) {
     });
 }
 
+//________________________________METHODS________________________________
+
+function putUserAuth(app){
+    return function(request, response) {
+        const id = request.params.id;
+        const pw = request.body.pw;
+        if (typeof id === undefined || typeof pw === undefined) {
+            sendStatus(app, request, response, "BAD_REQUEST");
+        }else{ //See if user exists in db
+            request.app.locals.model.users.getUser(id).
+            then((usr) =>{ //User exists, check pw
+                bcrypt.compare(pw, usr.hash).then((res) => { //Check if usr pswd is correct
+                    if(res === true){ //Pw is correct, generate and store updated token
+                        //console.log('CORRECT PW!');
+                        let token = generateToken();
+                        usr.token = token;
+                        usr.timeout = new Date().getTime()/1000;
+                        request.app.locals.model.users.updateUser(usr).then(()=>{ //User updated, send OK
+                            sendStatus(app, request, response, "OK", id, token);
+                        }).catch(()=>{ //Failed to find and update user 
+                            sendStatus(app, request, response, "NOT_FOUND", id);
+                        });
+                    }else{ //Pw is incorrect
+                        console.log('INCORRECT PW!');
+                        sendStatus(app, request, response, "UNAUTHORIZED", id);
+                    }
+                }).catch((err) =>{ //Issue with pw
+                    sendStatus(app, request, response, "UNAUTHORIZED", id);
+                    console.error(err);
+                });
+            }).catch((err)=>{ //If does not find user, send err  
+                sendStatus(app, request, response, "NOT_FOUND", id);
+                console.error(err);
+            });
+        }
+    }
+}
+
+function createUser(app){
+    return function(request, response) {
+        const id = request.params.id;
+        const pw = request.query.pw;
+        if (typeof id === undefined || typeof pw === undefined) {
+            sendStatus(app, request, response, "BAD_REQUEST");
+        }else{
+            request.app.locals.model.users.getUser(id). //Check if user already exists
+            then(() => { //USER ALREADY EXISTS
+                sendStatus(app, request, response, "SEE_OTHER", id);
+            }).catch(() => { //NEED TO CREATE USER
+                bcrypt.hash(pw,10) //Async hash password
+                .then((hash) => {
+                    let token = generateToken();
+                    let bod = createUserObject(id, hash, request.body, token);
+                    request.app.locals.model.users.newUser(bod). //Store usr obj in db
+                    then((id) => { //Send response to client
+                        sendStatus(app, request, response, "CREATED", id, token);
+                    }).catch((err) => { //Failed to create new user in db
+                        sendStatus(app, request, response, "SERVER_ERROR");
+                        console.error(err);
+                    });
+                }).catch((err) => { //Failed to hash password
+                    sendStatus(app, request, response, "SERVER_ERROR");
+                    console.error(err);
+                });
+            });
+        }
+    };
+}
+
+function getUserFun(app) {
+    return function(request, response) {
+        const id = request.params.id;
+        let auth = request.get("authorization")
+        auth = auth.split(" ")[1];
+        if (typeof id === 'undefined') {
+          sendStatus(app, request, response, "BAD_REQUEST")
+        }
+        else {
+            request.app.locals.model.users.getUser(id).
+            then((user) => { //Found user
+                let currTime = new Date().getTime()/1000;
+                let timedOut = Math.abs(user.timeout - currTime); 
+                if(user.token !== undefined && user.token == auth && timedOut < app.locals.authTimeout){
+                    response.send(user.body);
+                }
+                else{
+                    sendStatus(app, request, response, "UNAUTHORIZED", id)
+                }
+            }).
+            catch((err) => { //User not found
+                sendStatus(app, request, response, "NOT_FOUND", id)
+                console.error(err);
+            });
+        }
+    };
+}
+
+//________________________________HELPER FUNCTIONS________________________________
+
 function setupRoutes(app) {
     app.use(bodyParser.json());
     app.put('/users/:id/auth', putUserAuth(app));
     app.put('/users/:id', createUser(app));
     app.get('/users/:id', getUserFun(app));
+}
+
+function generateToken(){
+    //Trivial right now, need to make more secure
+    return Math.floor(Math.random()*4096*4096*4096);
+}
+
+function createUserObject(id, hash, reqBody, token = undefined) {
+    let bod = {};
+    bod._id = id;
+    bod.hash = hash;
+    if (token !== undefined){
+        bod.token = token;
+    }
+    bod.timeout = new Date().getTime()/1000;
+    if (reqBody != undefined ) {
+        bod.body = reqBody;
+    }
+    return bod;
 }
 
 function sendStatus(app, request, response, respStatus, id = undefined, token = undefined) {
@@ -82,137 +202,7 @@ function sendStatus(app, request, response, respStatus, id = undefined, token = 
     }
 }
 
-function generateToken(){
-    //Trivial right now, need to make more secure
-    return Math.floor(Math.random()*4096*4096*4096);
-}
-
-function putUserAuth(app){
-    return function(request, response) {
-        const id = request.params.id;
-        const pw = request.body.pw;
-        if (typeof id === undefined || typeof pw === undefined) {
-            sendStatus(app, request, response, "BAD_REQUEST");
-        }else{ //See if user exists in db
-            request.app.locals.model.users.getUser(id).
-            then((usr) =>{ //User exists, check pw
-                bcrypt.compare(pw, usr.hash).then((res) => { //Check if usr pswd is correct
-                    if(res === true){ //Pw is correct, generate and store updated token
-                        //console.log('CORRECT PW!');
-                        let token = generateToken();
-                        usr.token = token;
-                        usr.timeout = new Date().getTime()/1000;
-                        request.app.locals.model.users.updateUser(usr).then(()=>{ //User updated, send OK
-                            sendStatus(app, request, response, "OK", id, token);
-                        }).catch(()=>{ //Failed to find and update user 
-                            sendStatus(app, request, response, "NOT_FOUND", id);
-                        });
-                    }else{ //Pw is incorrect
-                        console.log('INCORRECT PW!');
-                        sendStatus(app, request, response, "UNAUTHORIZED", id);
-                    }
-                }).catch((err) =>{ //Issue with pw
-                    sendStatus(app, request, response, "UNAUTHORIZED", id);
-                    console.error(err);
-                });
-            }).catch((err)=>{ //If does not find user, send err  
-                sendStatus(app, request, response, "NOT_FOUND", id);
-                console.error(err);
-            });
-        }
-    }
-}
-
-function createUserObject(id, hash, reqBody, token = undefined) {
-    let bod = {};
-    bod._id = id;
-    bod.hash = hash;
-    if (token !== undefined){
-        bod.token = token;
-    }
-    bod.timeout = new Date().getTime()/1000;
-    if (reqBody != undefined ) {
-        bod.body = reqBody;
-    }
-    return bod;
-}
-
-function createUser(app){
-    return function(request, response) {
-        const id = request.params.id;
-        const pw = request.query.pw;
-        if (typeof id === undefined || typeof pw === undefined) {
-            console.log("Bad Request1");
-            sendStatus(app, request, response, "BAD_REQUEST");
-        }
-        else {
-            request.app.locals.model.users.getUser(id).
-            then(() => { //USER ALREADY EXISTS
-                console.log("IF SEND THIS"); // this
-                console.log("sent status1"); // this
-                sendStatus(app, request, response, "SEE_OTHER", id);
-            }).catch(() => { //NEED TO CREATE USER
-                console.log("SHOULDNT SEND THIS"); // this
-                //async hash password
-                console.log(pw);
-                bcrypt.hash(pw,10)
-                .then((hash) => {
-                    let token = Math.floor(Math.random()*4096*4096);
-                    let bod = createUserObject(id, hash, request.body, token);
-                    //Store usr obj in db
-                    request.app.locals.model.users.newUser(bod).
-                    then((id) => {
-                        //send response to client
-                        console.log("created1");
-                        sendStatus(app, request, response, "CREATED", id, token);
-                    }).catch((err) => {
-                        //Failed to create new user in db
-                        console.log("ServerErr1");
-                        sendStatus(app, request, response, "SERVER_ERROR");
-                        console.error(err);
-                    });
-                }).catch((err) => {
-                    //failed to hash password
-                    console.log("ServerErr2"); // this
-                    sendStatus(app, request, response, "SERVER_ERROR");
-                    console.error(err);
-                });
-            });
-        }
-    };
-}
-
-function getUserFun(app) {
-    return function(request, response) {
-        const id = request.params.id;
-        let auth = request.get("authorization")
-        auth = auth.split(" ")[1];
-        if (typeof id === 'undefined') {
-          sendStatus(app, request, response, "BAD_REQUEST")
-        }
-        else {
-            request.app.locals.model.users.getUser(id).
-            then((user) => {
-                //found user
-                let currTime = new Date().getTime()/1000;
-                let timedOut = Math.abs(user.timeout - currTime); 
-                if(user.token !== undefined && user.token == auth && timedOut < app.locals.authTimeout){
-                    //console.log("same!");
-                    response.send(user.body);
-                }
-                else{
-                    //console.log("different!");
-                    sendStatus(app, request, response, "UNAUTHORIZED", id)
-                }
-            }).
-            catch((err) => {
-                //user not found
-                sendStatus(app, request, response, "NOT_FOUND", id)
-                console.error(err);
-            });
-        }
-    };
-}
+//________________________________EXPORT________________________________
 
 module.exports = {
   serve: serve
