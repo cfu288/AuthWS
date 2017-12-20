8ca34df27ba56ba12c649b7741dd8117ac117230
Christopher Fu <cfu6@binghamton.edu>
2017-12-20 21:47:55 +0000

    added simple mocha tests for create user

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
-const axios = require('axios');
+const should = require('chai').should();
+const expect = require('chai').expect;
+const supertest = require('supertest');
+const url = 'https://35.190.149.215:80'; //'mongodb://localhost:27017/test_db';
+const api = supertest(url);
 
-const url = '35.190.149.215:80'; //'mongodb://localhost:27017/test_db';
 //const collection = 'tests'; //use users instead
 
 describe('Array', function() { //Test group array 
@@ -12,27 +15,31 @@ describe('Array', function() { //Test group array
     });
 });
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 describe('Create User', function() {
-    /*describe('User Already Exists', function() {
-        it('Should return 303 when user already exists', function(){ 
-            
+    it('Should return 201 when user does not already exist', function(done){
+        api.put('/users/usr1?pw=pass1')
+        .set('Accept', 'application/json')
+        .expect(201)
+        .end(function(err,res){
+            expect(res.body).to.have.property("status");
+            expect(res.body.status).to.equal("CREATED");
+            expect(res.body).to.have.property("auth_token");
+            expect(res.body.authToken).to.not.equal(null);
+            done();
         });
-    });*/
-    describe('User Does Not Exist Yet', function(){
-        it('Should return 201 when user does not already exist',
-        function(){
-            //console.log(`${url}/users/usr1?pw=$pass1`); 
-            return axios.put(`${url}/users/usr1?pw=$pass1`, {"payload":'testpayload'})
-            .then((response) => { //response.data, response.headers
-                console.error(response);
-                assert.strictEqual(201, response.status);
-                done();
-            })
-            .catch((err) => {
-                if (err) { assert.strictEqual(201, err.status); }
-                console.error(err);
-                done();
-            });
-        });
+    });
+    it('Should return 303 when user already exists', function(){ 
+         api.put('/users/usr1?pw=pass1')
+        .set('Accept', 'application/json')
+        .expect(303)
+        .end(function(err,res){
+            expect(res.body).to.have.property("status");
+            expect(res.body.status).to.equal("EXISTS");
+            expect(res.body).to.have.property("info");
+            expect(res.body.info).to.equal("user usr1 already exists");
+            done();
+        });    
     });
 });
